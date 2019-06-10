067c215d79dcb5a0282e1dc252307fb34930921a
qianzong330 <360644740@qq.com>
2019-06-10 17:43:13 +0800

    提价

--- demo/src/com/heima/test.java
+++ demo/src/com/heima/test.java
@@ -3,9 +3,6 @@ package com.heima;
 public class test {
     public static void main(String[] args) {
         System.out.println(111);
-        System.out.println(33333 );
-        System.out.println(555);
-        System.out.println(666);
-        System.out.println(777);
+
     }
 }
